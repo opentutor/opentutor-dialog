5caa92d0ca9ed77b7584750aa777f04a41400cf4
rushsangs <rush.sang@rutgers.edu>
2020-06-01 08:34:09 -0600

    basic framework for back and forth messages built

--- node/src/routes/api/session.ts
+++ node/src/routes/api/session.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
-import AutoTutorQuestion from 'models/AutoTutorQuestion';
+import AutoTutorData from 'models/AutoTutorData';
+//import AutoTutorOutput from "models/AutoTutorOutput";
 
 const router = express.Router({ mergeParams: true });
 
+//This is the array that has hardcoded dialogs
+var dialogs = [
+    'q1',
+    'q2',
+    'q3'
+];
+
 router.post('/', (req: Request, res: Response) => {
+
     //if there is no session ID, send error.
     if(req.body['Id'] == null) {
         res.status(400)
@@ -11,6 +20,7 @@ router.post('/', (req: Request, res: Response) => {
         return;
     }
 
+    //session start packet
     var jsonData = {
         'Id': req.body['Id'],
         'User': req.body['User'],
@@ -22,10 +32,33 @@ router.post('/', (req: Request, res: Response) => {
 
     //TODO: add in mechanics to extract prompt question from the script itself
 
-    var atq = new AutoTutorQuestion();
+
+
+    var atd = new AutoTutorData();
+
+    // var ato = new AutoTutorOutput();
+
     res.send({
         status: 'ok',
-        "data": atq.convertToJson()
+        "data": atd.convertToJson(),
+        'dialog': dialogs[0],
+        'turn': 0
+    });
+});
+
+router.post('/dialog', (req: Request, res: Response) => {
+    //if there is no turn number, send error.
+    if(req.body['turn'] == null) {
+        res.status(400)
+            .send();
+        return;
+    }
+    console.log('message was ' + req.body['message'])
+    var turn = req.body['turn'];
+    res.send({
+        status: 'ok',
+        'dialog': dialogs[turn+1],
+        'turn': turn+1
     });
 });
 
