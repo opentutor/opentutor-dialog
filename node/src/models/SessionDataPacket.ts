4c285bafb3cac6a40ec22f9394a9567fdc698b4e
rushsangs <rush.sang@rutgers.edu>
2020-06-04 14:06:01 -0600

    temp code

---
+++ node/src/models/SessionDataPacket.ts
@@ -0,0 +1,17 @@
+export default class SessionDataPacket
+{
+    sessionID: string;
+    sessionHistory: string;
+    previousUserResponse: string;
+    previousSystemResponse: string;
+
+};
+
+class SessionHistory
+{
+    userResponses: string[];
+    userScores: number[];
+    systemResponses: string[];
+
+    hash: 
+}
