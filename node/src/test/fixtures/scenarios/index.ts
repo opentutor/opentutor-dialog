61ff9859d0845cac0731fa90fe578f2752f0cf25
rushsangs <rush.sang@rutgers.edu>
2020-06-26 10:36:58 -0600

    format

--- node/src/test/fixtures/scenarios/index.ts
+++ node/src/test/fixtures/scenarios/index.ts
@@ -5,7 +5,7 @@ import lesson1p2 from './lesson1-p2';
 import lesson1p3 from './lesson1-p3';
 import lesson1p4 from './lesson1-p4';
 import lesson1p5 from './lesson1-p5';
-import lesson1p6 from  './lesson1-p6';
+import lesson1p6 from './lesson1-p6';
 
 export const all: DialogScenario[] = [
   lesson1p1,
