41d054dfd22d5c77b911a5276499e62780efa96a
rushsangs <rush.sang@rutgers.edu>
2020-06-26 10:05:49 -0600

    added testcase to test path when prompt answered correctly

--- node/src/test/fixtures/scenarios/index.ts
+++ node/src/test/fixtures/scenarios/index.ts
@@ -4,11 +4,13 @@ import lesson1p1 from './lesson1-p1';
 import lesson1p2 from './lesson1-p2';
 import lesson1p3 from './lesson1-p3';
 import lesson1p4 from './lesson1-p4';
+import lesson1p5 from './lesson1-p5';
 
 export const all: DialogScenario[] = [
   lesson1p1,
   lesson1p2,
   lesson1p3,
   lesson1p4,
+  lesson1p5,
 ];
 export default all;
---
+++ node/src/test/fixtures/scenarios/lesson1-p5.ts
@@ -0,0 +1,109 @@
+import { DialogScenario } from 'test/fixtures/types';
+
+export const scenario: DialogScenario = {
+  name: 'lesson1 part 5: this does hints and prompts',
+  lessonId: 'l1',
+  expectedRequestResponses: [
+    {
+      userInput: 'Rules apply differently to the group',
+      mockClassifierResponse: {
+        data: {
+          output: {
+            expectationResults: [
+              { evaluation: 'Bad', score: 1.0 },
+              { evaluation: 'Neutral', score: 0.0 },
+              { evaluation: 'Neutral', score: 0.0 },
+            ],
+          },
+        },
+      },
+      expectedResponse: [
+        {
+          author: 'them',
+          type: 'text',
+          data: {
+            text:
+              'Some people get confused at this point. Try typing whatever you are thinking and we will go from there.',
+          },
+        },
+        {
+          author: 'them',
+          type: 'text',
+          data: {
+            text: 'Consider this.',
+          },
+        },
+        {
+          author: 'them',
+          type: 'text',
+          data: {
+            text:
+              'Why might you allow bad behavior in a group that you normally would not allow yourself to do?',
+          },
+        },
+      ],
+    },
+    {
+      userInput: "I don't know",
+      mockClassifierResponse: {
+        data: {
+          output: {
+            expectationResults: [
+              { evaluation: 'Neutral', score: 0.0 },
+              { evaluation: 'Neutral', score: 0.0 },
+              { evaluation: 'Neutral', score: 0.0 },
+            ],
+          },
+        },
+      },
+      expectedResponse: [
+        {
+          author: 'them',
+          type: 'text',
+          data: {
+            text: 'See if you can get this',
+          },
+        },
+        {
+          author: 'them',
+          type: 'text',
+          data: {
+            text: 'What might cause you to lower your standards?',
+          },
+        },
+      ],
+    },
+    {
+      userInput: 'peer pressure',
+      mockClassifierResponse: {
+        data: {
+          output: {
+            expectationResults: [
+              { evaluation: 'Good', score: 1.0 },
+              { evaluation: 'Neutral', score: 0.0 },
+              { evaluation: 'Neutral', score: 0.0 },
+            ],
+          },
+        },
+      },
+      expectedResponse: [
+        {
+          author: 'them',
+          type: 'text',
+          data: {
+            text: 'Great',
+          },
+        },
+        {
+          author: 'them',
+          type: 'text',
+          data: {
+            text: 'How can it affect someone when you correct their behavior?',
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export default scenario;
