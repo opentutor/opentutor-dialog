bd7ac3648bb2ed3544a495b68d31a91778e449c5
rushsangs <rush.sang@rutgers.edu>
2020-08-06 18:29:35 -0600

    code review fixes

--- node/src/routes/dialog.ts
+++ node/src/routes/dialog.ts
@@ -1,3 +1,9 @@
+/*
+This software is Copyright ©️ 2020 The University of Southern California. All Rights Reserved. 
+Permission to use, copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice and subject to the full license file found in the root of this software deliverable. Permission to make commercial use of this software may be obtained by contacting:  USC Stevens Center for Innovation University of Southern California 1150 S. Olive Street, Suite 2300, Los Angeles, CA 90115, USA Email: accounting@stevens.usc.edu
+
+The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
+*/
 import express, { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
 import OpenTutorData, {
@@ -43,9 +49,9 @@ router.post(
         return next(createError(400, error));
       }
       const lessonId = req.params['lessonId'];
-      let atd: OpenTutorData;
-      //request graphQL for data.
-      atd = convertLessonDataToATData(await getLessonData(lessonId));
+      const atd: OpenTutorData = convertLessonDataToATData(
+        await getLessonData(lessonId)
+      );
       //new sessionDataPacket
       const sdp = newSession(atd, body.sessionId);
       addTutorDialog(sdp, beginDialog(atd));
@@ -78,8 +84,9 @@ router.post(
         return res.status(410).send();
       }
 
-      let atd: OpenTutorData;
-      atd = convertLessonDataToATData(await getLessonData(lessonId));
+      const atd: OpenTutorData = convertLessonDataToATData(
+        await getLessonData(lessonId)
+      );
       if (!atd) return res.status(404).send();
       addUserDialog(sessionData, req.body['message']);
       const msg = await processUserResponse(lessonId, atd, sessionData);
