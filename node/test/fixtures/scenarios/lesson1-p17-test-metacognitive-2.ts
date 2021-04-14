a198a53020a3047473447e809dfcd14c7522b727
auerbach <auerbach@ict.usc.edu>
2021-04-14 15:52:07 -0700

    added second metacognitive test case.

---
+++ node/test/fixtures/scenarios/lesson1-p17-test-metacognitive-2.ts
@@ -0,0 +1,40 @@
+import { DialogScenario } from 'test/fixtures/types';
+import { Evaluation } from 'apis/classifier';
+import { ResponseType } from 'dialog/response-data';
+
+//navy integrity perfect answer
+export const scenario: DialogScenario = {
+  name: 'lesson1 part 17: metacognitive response test 2',
+  lessonId: 'q1',
+  expectedRequestResponses: [
+    {
+      userInput: 'I dont know',
+      mockClassifierResponse: {
+        data: {
+          output: {
+            expectationResults: [
+              { evaluation: Evaluation.Good, score: 1.0 },
+              { evaluation: Evaluation.Good, score: 0.5 },
+              { evaluation: Evaluation.Good, score: 0.5 },
+            ],
+            speechActs: {
+              metacognitive: { evaluation: Evaluation.Good, score: 1.0 },
+              profanity: { evaluation: Evaluation.Good, score: 0.5 },
+            },
+          },
+        },
+      },
+      expectedResponse: [
+        {
+          author: 'them',
+          type: ResponseType.FeedbackPositive,
+          data: {
+            text: 'Great.',
+          },
+        },
+      ],
+    },
+  ],
+};
+
+export default scenario;
