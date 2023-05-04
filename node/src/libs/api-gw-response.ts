4b072dae68049ce348591e9fdcce7428cb2ca820
Daniel Auerbach <dan100110@users.noreply.github.com>
2023-05-04 15:48:42 -0700

    Serverless (#93)
    added serverless.yml and handlers, need to work on tests
    
    
    ---------
    
    Co-authored-by: aaronshiel <ashiel408@gmail.com>

--- node/src/libs/api-gw-response.ts
+++ node/src/libs/api-gw-response.ts
@@ -5,6 +5,18 @@ Permission to use, copy, modify, and distribute this software and its documentat
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
 
-export function getApiKey(): string {
-  return process.env.API_SECRET;
+interface FormatJSONResponse {
+  statusCode: number;
+  body: string;
 }
+
+// eslint-disable-line @typescript-eslint/no-explicit-any
+export const formatJSONResponse = (
+  response: any,
+  statusCode = 200
+): FormatJSONResponse => {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(response),
+  };
+};
